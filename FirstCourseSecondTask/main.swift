79e382c5ff9086a13584f30e66019466f93cb1a1
Alexander Kolovatov <a.kolovatov@gmail.com>
2019-11-28 22:47:20 +0600

    Initial commit

---
+++ FirstCourseSecondTask/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  FirstCourseSecondTask
+//
+//  Copyright © 2017 E-Legion. All rights reserved.
+//
+
+import Foundation
+import FirstCourseSecondTaskChecker
+
+
+let checker = Checker()
+
+
+checker.checkFirstFunction(function: <#T##([Int]) -> (Int, Int)#>)
+
+
+checker.checkSecondFunction(function: <#T##([Checker.Circle]) -> [Checker.Circle]#>)
+
+
+checker.checkThirdFunction(function: <#T##([Checker.EmployeeData]) -> [Checker.Employee]#>)
+
+
+checker.checkFourthFunction(function: <#T##([String]) -> [String : [String]]#>)
